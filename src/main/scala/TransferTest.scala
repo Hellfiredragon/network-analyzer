da5149ea3bc0d361172c0d9157f3f91520a72eac
Jan Muthreich <jan@muthreich.eu>
2021-01-08 15:40:18 +0100

    clean ups
    Former-commit-id: ede50bb72a548aedb9c318cb25b05b80d3989f74 [formerly e4b57a17978fcff78f5b1448cb78a64bc55f7332]
    Former-commit-id: 63e7bf3523efb7776e197e4cd9395505415cc124

--- src/main/scala/TransferTest.scala
+++ src/main/scala/TransferTest.scala
@@ -1,5 +1,107 @@
-/**
-  * Created by Jan on 12.05.2016.
-  */
-class TransferTest {
+import akka.stream.scaladsl._
+import java.net.InetSocketAddress
+import java.nio.ByteOrder
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.io.IO
+import akka.stream.actor.ActorSubscriber
+import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
+import akka.util.{ByteString, ByteStringBuilder}
+import scala.concurrent.Future
+
+class ServerHandler extends Actor {
+
+    import akka.io.Tcp._
+
+    def receive = {
+
+        case Received(data) => println(data)
+        case PeerClosed => context stop self
+    }
+}
+
+class ServerActor extends Actor {
+
+    import akka.io.Tcp._
+    import context.system
+
+    IO(akka.io.Tcp) ! Bind(self, new InetSocketAddress("localhost", 10000))
+
+    def receive = {
+
+        case b@Bound(localAddress) =>
+        case CommandFailed(_: Bind) => context stop self
+        case b@Connected(remote, local) =>
+            println("server", remote, local)
+            val handler = context.actorOf(Props[ServerHandler])
+            val connection = sender()
+            connection ! Register(handler)
+        case x => println("unknown", x)
+    }
+}
+
+class StringActor extends Actor {
+
+    def receive = {
+
+        case x => println(x)
+    }
+}
+
+object Server extends App {
+
+    implicit val system = ActorSystem("server")
+
+    implicit val materializer = akka.stream.ActorMaterializer()
+
+    //system.actorOf(Props[ServerActor])
+    val connections: Source[IncomingConnection, Future[ServerBinding]] = Tcp().bind("localhost", 10000)
+
+    connections runForeach { connection =>
+        println(connection)
+
+        val sub = ActorSubscriber[String](system.actorOf(Props[StringActor]))
+
+        val printer = Flow[ByteString]
+            .via(Framing.delimiter(ByteString("\r\n"), maximumFrameLength = 4, allowTruncation = true))
+            .map(_.utf8String)
+            .to(Sink.fromSubscriber(sub))
+
+        connection.flow.to(Sink.foreach(println))
+        println(printer)
+    }
+
+    println("started")
+}
+
+class ClientActor extends Actor {
+
+    import akka.io.Tcp._
+    import context.system
+
+    IO(akka.io.Tcp) ! Connect(new InetSocketAddress("localhost", 10000))
+
+    case object Ack extends Event
+
+    def receive = {
+
+        case CommandFailed(_: Connect) => context stop self
+        case c@Connected(remote, local) =>
+            println("client", remote, local)
+            val connection = sender()
+            context watch connection
+            connection ! Register(self)
+            for (i <- 1 to 100) {
+                connection ! Write(ByteString("test\r\n"), Ack)
+            }
+            println("written")
+    }
+}
+
+object Client extends App {
+
+    implicit val system = ActorSystem("client")
+
+    system.actorOf(Props[ClientActor])
+
+    println("started")
 }
--- src/test/scala/analyzer/pcap/PcapSpecs.scala
+++ src/test/scala/analyzer/pcap/PcapSpecs.scala
@@ -4,7 +4,7 @@ import java.io.EOFException
 import java.util.concurrent.TimeoutException
 
 import com.sun.nio.sctp.SctpChannel
-import org.pcap4j.core.Pcaps
+import org.pcap4j.core.{PcapHandle, Pcaps}
 import org.pcap4j.packet._
 import org.pcap4j.packet.TcpPacket.TcpHeader
 import org.scalatest.concurrent.ScalaFutures
@@ -15,45 +15,38 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class PcapSpecs
     extends FlatSpec
-    with Matchers
-    with ScalaFutures {
+        with Matchers
+        with ScalaFutures {
 
     "A Pcap Lib" should "do something" in {
+        val handle = Pcaps openOffline "traces/tcp-ecn-sample.pcap"
 
-        val handle = Pcaps.openOffline("traces/tcp-ecn-sample.pcap")
+        var packet: Packet = null
 
-        def loop() {
-
-            while (true) {
-                try {
-                    val packet = handle.getNextPacketEx
-                    packet match {
-                        case p: EthernetPacket => {
-                            System.out.println(p.getHeader)
-                            p.getPayload match {
-                                case ipv4: IpV4Packet => {
-                                    System.out.println(ipv4.getHeader)
-                                    ipv4.getPayload match {
-                                        case tcp: TcpPacket => System.out.println(tcp)
-                                        case udp: UdpPacket => System.out.println(udp)
-                                    }
-                                }
-                                case ipv6: IpV6Packet => {
-                                    System.out.println(ipv6.getHeader)
-                                }
+        while ( {
+            packet = handle getNextPacket; packet != null
+        }) packet match {
+            case p: EthernetPacket => {
+                //println(p.getHeader)
+                p.getPayload match {
+                    case ipv4: IpV4Packet => {
+                        //println(ipv4.getHeader)
+                        ipv4.getPayload match {
+                            case tcp: TcpPacket => {
+                                //println(tcp)
+                                println(if(tcp.getPayload != null) tcp.getPayload.getClass)
                             }
+                            case udp: UdpPacket => println(udp)
                         }
                     }
-                } catch {
-                    case e: TimeoutException => System.out.println("Timeout")
-                    case e: EOFException => return
+                    case ipv6: IpV6Packet => {
+                        println(ipv6.getHeader)
+                    }
                 }
             }
         }
 
-        loop()
 
         handle.close();
-
     }
 }
